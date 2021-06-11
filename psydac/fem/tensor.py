72ba77d374011fd3d672c9704c94d78dd72668d6
David Schneller <david.schneller@tum.de>
2021-06-11 18:48:34 +0200

    First fix

--- psydac/fem/tensor.py
+++ psydac/fem/tensor.py
@@ -154,7 +154,21 @@ class TensorFemSpace( FemSpace ):
         if not field.coeffs.ghost_regions_in_sync:
             field.coeffs.update_ghost_regions()
 
+        skip = False
         for (x, xlim, space) in zip( eta, self.eta_lims, self.spaces ):
+
+            domain = space.domain
+
+            if space.periodic:
+                domainsize = domain[1] - domain[0]
+                while domain[0] > x:
+                    x += domainsize
+                while domain[1] <= x:
+                    x -= domainsize
+
+            if x > domain[1] or x < domain[0]:
+                skip = True
+                break
 
             knots  = space.knots
             degree = space.degree
@@ -166,7 +180,7 @@ class TensorFemSpace( FemSpace ):
             # TODO: Use local knot sequence instead of global #
             #       one to get correct span in all situations #
             #-------------------------------------------------#
-            if x == xlim[1] and x != knots[-1-degree]:
+            if x == domain[1] and x != knots[-1-degree]:
                 span -= 1
             #-------------------------------------------------#
             basis  = basis_funs( knots, degree, x, span )
@@ -175,40 +189,39 @@ class TensorFemSpace( FemSpace ):
             if space.basis == 'M':
                 basis *= space.scaling_array[span-degree : span+1]
 
-            # Determine local span
-            wrap_x   = space.periodic and x > xlim[1]
-            loc_span = span - space.nbasis if wrap_x else span
-
             bases.append( basis )
-            index.append( slice( loc_span-degree, loc_span+1 ) )
+            index.append( slice( span-degree, span+1 ) )
 
-        # Get contiguous copy of the spline coefficients required for evaluation
-        index  = tuple( index )
-        coeffs = field.coeffs[index].copy()
-        if weights:
-            coeffs *= weights[index]
+        if skip:
+            return np.zeros((), dtype=field.coeffs.dtype)
+        else:
+            # Get contiguous copy of the spline coefficients required for evaluation
+            index  = tuple( index )
+            coeffs = field.coeffs[index].copy()
+            if weights:
+                coeffs *= weights[index]
 
-        # Evaluation of multi-dimensional spline
-        # TODO: optimize
+            # Evaluation of multi-dimensional spline
+            # TODO: optimize
 
-        # Option 1: contract indices one by one and store intermediate results
-        #   - Pros: small number of Python iterations = ldim
-        #   - Cons: we create ldim-1 temporary objects of decreasing size
-        #
-        res = coeffs
-        for basis in bases[::-1]:
-            res = np.dot( res, basis )
+            # Option 1: contract indices one by one and store intermediate results
+            #   - Pros: small number of Python iterations = ldim
+            #   - Cons: we create ldim-1 temporary objects of decreasing size
+            #
+            res = coeffs
+            for basis in bases[::-1]:
+                res = np.dot( res, basis )
 
-#        # Option 2: cycle over each element of 'coeffs' (touched only once)
-#        #   - Pros: no temporary objects are created
-#        #   - Cons: large number of Python iterations = number of elements in 'coeffs'
-#        #
-#        res = 0.0
-#        for idx,c in np.ndenumerate( coeffs ):
-#            ndbasis = np.prod( [b[i] for i,b in zip( idx, bases )] )
-#            res    += c * ndbasis
+    #        # Option 2: cycle over each element of 'coeffs' (touched only once)
+    #        #   - Pros: no temporary objects are created
+    #        #   - Cons: large number of Python iterations = number of elements in 'coeffs'
+    #        #
+    #        res = 0.0
+    #        for idx,c in np.ndenumerate( coeffs ):
+    #            ndbasis = np.prod( [b[i] for i,b in zip( idx, bases )] )
+    #            res    += c * ndbasis
 
-        return res
+            return res
 
     # ...
     def eval_field_gradient( self, field, *eta , weights=None):
